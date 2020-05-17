822805601dc518562d720b22d9ee7d7c602e87c0
Sergey Shovkoplyas <shovkoplyassergey@gmail.com>
2020-05-17 22:59:52 +0300

    Add first page

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,47 @@
-window.onload = function () {
-	
-	console.log('js is working!!!'); 
 
-}
+$( document ).ready(function() {
 
-/* $( document ).ready(function() {
+	// ----------------- open/close nav on mobile-------------
 
-	console.log( 'JQuery!!!' );
+	$('.header__nav-btn').on('click', function () {
+		$('.header__nav-btn .nav-btn').toggleClass('active');
+		$('.header__nav').toggleClass('active');
+		$('body').toggleClass('locked');
+	});
 
-}); */
+	// ----------------- change language -------------
+
+	$('.language-btn').on('click', function(){
+		$(this).next('.language').toggleClass('active');
+	});
+
+	// -------------------- initialize slider on mobile --------------------
+
+	$('.section-2__slider').slick({
+		arrows: false,
+		infinite: false,
+		mobileFirst: true,
+		dots: true,
+    responsive: [
+			{
+				breakpoint: 767,
+				settings: 'unslick'
+			}
+    ]
+	});
+
+	$('.section-5__slider').slick({
+		arrows: false,
+		infinite: false,
+		mobileFirst: true,
+		slidesToShow: 1,
+		variableWidth: true,
+    responsive: [
+			{
+				breakpoint: 767,
+				settings: 'unslick'
+			}
+    ]
+	});
+
+});
